ef6ce200ea40e906313b5d6c03420a548112d873
LwinMoeOo <lwinmoeoo3112@gmail.com>
2025-04-29 16:43:41 +0700

    add side bar menu fe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,60 @@
 <template>
   <div class="side-bar">
     <div class="user">
-      <img src="../assets/user-profile.svg">
+      <img class="user-profile" src="../assets/user-profile.svg">
       <h2 class="user-name">Lwin Moe Oo</h2>
+    </div>
+    <div class="nav-items-container">
+      <ul class="nav-items">
+        <li class="nav-item"><img src="../assets/dashboard.svg" class="nav-icon">Dashboard</li>
+        <li class="nav-item"><img src="../assets/feed.svg" class="nav-icon"> Feed</li>
+        <li class="nav-item"><img src="../assets/message.svg" class="nav-icon">Inbox</li>
+        <li class="nav-item"><img src="../assets/create-post.svg" class="nav-icon">Create Post</li>
+        <li class="nav-item"><img src="../assets/manage-posts.svg" class="nav-icon">Manage Post</li>
+        <li class="nav-item"><img src="../assets/save-posts.svg" class="nav-icon">Save Post(s)</li>
+        <li class="nav-item"><img src="../assets/note.svg" class="nav-icon">Note</li>
+        <li class="nav-item"><img src="../assets/settings.svg" class="nav-icon">Setting</li>
+      </ul>
     </div>
   </div>
 
-  <div class="login">
+  <div v-if="accountInUse" class="signout">
+    <button @click="signOut">Sign Out</button>
+  </div>
+
+  <div v-else class="login">
     <router-link :to="{ name: 'user-form' }"><button>Login</button></router-link>
   </div>
 
   <div class="add-content">
-    <!-- <img src="" alt=""> -->
+    <img class="add-content-btn" src="../assets/add-button.svg">
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'HomeView',
   components: {
 
   },
+  setup() {
+    let accountInUse = ref(false)
+
+    let signOut = () => {
+      console.log("Sign Process")
+    }
+
+    return { accountInUse, signOut }
+  }
 });
 </script>
 
 <style>
 .side-bar {
-  /* background-color: aquamarine; */
   background-color: rgb(123, 166, 197);
+  position: sticky;
   float: left;
   height: 100vh;
   width: 300px;
@@ -38,24 +63,99 @@ export default defineComponent({
 .user {
   width: 100%;
   margin: 20px 5px;
-  text-align: center;
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: center;
+  align-items: center;
 }
 
 .user-name {
   display: inline-block;
-  /* margin-left: auto 3px; */
+  font-size: 20px;
+  margin-left: 10px;
 }
 
-img {
-  width: 60px;
-  height: 60px;
+.user-profile {
+  width: 30px;
+  height: 30px;
 }
 
-.login {
+.nav-items-container {
+  height: 55%;
+}
+
+.nav-items {
+  width: 80%;
+  height: 100%;
+  margin: 20px auto;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-content: center;
+}
+
+.nav-icon {
+  width: 20px;
+  height: 20px;
+  margin-right: 7px;
+}
+
+.nav-item {
+  margin-left: 20px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+}
+
+.login,
+.signout {
   float: right;
   margin: 20px;
 }
+
+.add-content {
+  display: none;
+}
+
+.nav-item:hover {
+  font-weight: bold;
+}
+
+@media (max-width: 1440px) {
+  .add-content {
+    /* display: block;
+    height: 100vh;
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: flex-end;
+    margin: 20px; */
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+  }
+
+  .add-content-btn {
+    width: 30px;
+    height: 30px;
+    background-color: rgb(123, 166, 197);
+    padding: 10px;
+    border-radius: 50px;
+    color: aliceblue;
+  }
+}
+
+/* @media (max-width: 1440px) {
+  .side-bar {
+    display: none;
+  }
+}
+
+@media (max-width: 1440px) {
+  .login {
+    display: none;
+  }
+} */
 </style>
